af7f92c0c3bcd936529998623438399a0d8c4044
CoderSaaransh <87510642+CoderSaaransh@users.noreply.github.com>
2021-07-16 08:58:22 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the speaker in the poem??",
+    answer: "THE SPEAKER IS NOT IDENTIFIED",
+    options: [
+      "THE TRUE GODS",
+      "PAN",
+      "SYRINX",
+      "THE SPEAKER IS NOT IDENTIFIED"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In which stanza is there foreshadowing of dark events to come?",
+    answer: "1",
+    options: [
+      "3",
+      "2",
+      "4",
+      "1"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the meaning of BAN in the poem?",
+    answer: "A MALDECTION",
+    options: [
+      "A MALDECTION",
+      "TO PROHIBIT",
+      "AN OFFICIAL ORDER",
+    ]
+  },
+    {
+    numb: 4,
+    question: "And the broken lilies a-dying lay is an example of:",
+    answer: "PERSONIFICATION",
+    options: [
+      "PERSONIFICATION",
+      "SIMILE",
+      "METAPHOR",
+      "SKIP"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "The half-god Pan is half human and half:",
+    answer: "GOAT",
+    options: [
+      "GOAT",
+      "CAMEL",
+      "HORSE",
+      "DONKEY"
+    ]
+    
+  },
+   
+//     {
+//     numb: 6,
+//     question: "Your Question is Here",
+//     answer: "Correct answer of the question is here",
+//     options: [
+//       "Option 1",
+//       "option 2",
+//       "option 3",
+//       "option 4"
+//     ]
+// },
+];
